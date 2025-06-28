fa48b894661bad287e4d7195cb1de97186b3cc1d
jithesh79 <jitheshks11@gmail.com>
2025-06-28 20:31:28 +0530

    code commited

--- loop_logs.sh
+++ loop_logs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 USERID=$(id -u)
-R="e\[31m"
-G="e\[32m"
-Y="e\[33m"
-N="e\[0m"
+R="\e[31m"   # Red
+G="\e[32m"   # Green
+Y="\e[33m"   # Yellow
+N="\e[0m"    # Reset
 LOGS_FOLDER="/var/logs/shellscript.sh"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -11,37 +12,31 @@ PACKAGES=("mysql" "python" "nginx" "java")
 
 mkdir -p $LOGS_FOLDER  
 
-echo "Script Stating date : $(date)" &>>$LOG_FILE
- 
- if [ $USERID -ne 0 ]
- then
-    echo -e "$R please run this script by using roor user $N"  | tee -a &>>$LOG_FILE 
+echo "Script Starting date: $(date)" >> $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script as root user $N" | tee -a $LOG_FILE
     exit 1
- else
-    echo "Your runing with root user" &>>$LOG_FILE
- fi
+else
+    echo "You are running as root user" >> $LOG_FILE
+fi
 
 VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "installing $2 is.....$G SUCCESS $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE 
+    if [ $1 -eq 0 ]; then
+        echo -e "Installing $2...$G SUCCESS $N" >> $LOG_FILE
     else
-        echo -e "installing $2 is......$R FAILUR $N" | tee -a &>>$LOG_FILE 
+        echo -e "Installing $2...$R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
-for package in ${PACKAGES [@]}
-do
-    dnf list installed $package &>>$LOG_FILE
-    if [ $? -ne 0 ]
-    then
-        echo "$package is not installed...go to install" | tee -a &>>$LOG_FILE 
-        dnf install $package -y  | tee -a &>>$LOG_FILE 
+for package in ${PACKAGES[@]}; do
+    dnf list installed $package >> $LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed... Installing now" | tee -a $LOG_FILE
+        dnf install $package -y | tee -a $LOG_FILE
         VALIDATE $? "$package"
     else
-        echo -e "Noting to do... $Y already installed $N"  | tee -a &>>$LOG_FILE 
+        echo -e "Nothing to do... $Y $package already installed $N" | tee -a $LOG_FILE
     fi
-
 done
-
