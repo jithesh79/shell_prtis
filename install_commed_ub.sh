a75b083a19a4d3dc0bbe4bded9824f755a42bd13
jithesh79 <jitheshks11@gmail.com>
2025-06-28 10:12:04 +0530

    code commited

--- install_commed_ub.sh
+++ install_commed_ub.sh
@@ -2,29 +2,26 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this Script by using root user"
+if [ "$USERID" -ne 0 ]; then
+    echo "Please run this script as root."
     exit 1
 else
-    echo "script running with root user"
+    echo "Script is running with root privileges."
 fi
 
-apt list installed mysql
+# Check if MySQL Server is installed
+dpkg -s mysql-server &> /dev/null
 
-if [ $? -ne 0 ]
-then
-    echo "MySql is not installed.....go to install it"
-    apt install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing MySQL......SUCCESs"
+if [ $? -ne 0 ]; then
+    echo "MySQL is not installed. Proceeding to install..."
+    apt update && apt install mysql-server -y
+    if [ $? -eq 0 ]; then
+        echo "Installing MySQL... SUCCESS"
     else
-        echo " Installing MySQL ......FAILUER"
+        echo "Installing MySQL... FAILURE"
         exit 1
     fi
-
 else
-    echo "MySQL is alredy installed..Noting to do"
-
+    echo "MySQL is already installed. Nothing to do."
 fi
+
