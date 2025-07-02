6016c89a82bc8dce40a10f591a567ed84e01d170
jithesh79 <jitheshks11@gmail.com>
2025-07-02 18:53:06 +0530

    code commited

--- shell-roboshop/mongodb.sh
+++ shell-roboshop/mongodb.sh
@@ -1,46 +1,44 @@
+#!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/logs/logsfile.sh"
-SCRIPT_NAME=$(echo $0 | cut "." -f1)
+LOGS_FOLDER="/var/log/logsfile.sh"
+SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Scripting starting date : $(date)"  | tee >>$LOG_FILE
+echo "Scripting starting date : $(date)"  | tee -a $LOG_FILE
 
-if [$USERID -ne 0 ]
-then
-    echo -e "$R Please run this Script by root user $N " >>$LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this Script as root user $N" | tee -a $LOG_FILE
     exit 1
 else
-    echo "your run this Script By root user" >>$LOG_FILE
-
+    echo "You are running this script as root user" | tee -a $LOG_FILE
 fi
 
-VALATADE (){
-    if [ $1 -eq o ]
-    then
-        echo -e " $2...$G Success $N " | tee >>$LOG_FILE
+VALIDATE () {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2...$G Success $N" | tee -a $LOG_FILE
     else
-        echo -e "  $2...$R Failner $N " | tee >>$LOG_FILE
+        echo -e "$2...$R Failed $N" | tee -a $LOG_FILE
         exit 1
     fi
-
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALATADE $? "Copying MongoDb repo "
+VALIDATE $? "Copying MongoDB repo"
 
-dnf mongo-org -y &>>$LOG_FILE
-VALATADE $? "Installing mongo db server"
+dnf install -y mongodb-org &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB server"
 
-systemctl enable mongodb &>>$LOG_FILE
-VALATADE $? "Starting Mongodb"
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB service"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALATADE $? "Editing Mongo db config file and editing remot conntion "
+VALIDATE $? "Updating MongoDB bind IP for remote access"
 
-systemctl restart mongodb &>>$LOG_FILE
-VALATADE $? "Restating mongodb"
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB"
